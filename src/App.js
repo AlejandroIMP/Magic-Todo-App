0c123b45dacb546ed1b5eeec39ffb26275acfe4c
AlejandroIMP <alejandrosunuc@gmail.com>
2024-07-11 19:52:41 -0600

    Se agregan las modificaciones, estilos y componentes al archivo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './platzi.webp';
-import './App.css';
+import { TodoCount } from './TodoCount/TodoCount';
+import { TodoInputSearch } from './TodoInputSearch/TodoInputSearch';
+import { TodoList } from './TodoList/TodoList';
+import { TodoItem } from './TodoItem/TodoItem';
+import { TodoButton } from './TodoButton/TodoButton';
+import { TodoTitle } from './TodoTitle/TodoTitle';
+import './styles.css';
+import React from 'react';
+
+const defaultTodos = [
+  {text: 'Take a Shower', description: "Take a laarge shower at 9 o'clock", date: '01/01/24', completed: true}, 
+  {text: 'Terminar el curso de React', description: "Finish all videos of the course ", date: '01/01/24', completed: false},
+  {text: 'Llorar con la llorona', description: "All day, everyday", date: '01/01/24', completed: true},
+  {text: 'Ola', description: "Ola", date: '01/01/24', completed: true}
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='todo--container'>
+
+      <TodoTitle/>
+      <TodoInputSearch/>
+
+      <TodoList>
+        {defaultTodos.map(todo => (
+          <TodoItem 
+          key={todo.text} 
+          text={todo.text}
+          description={todo.description}
+          date={todo.date}
+          completed ={todo.completed}
+          />
+        ))}
+        
+      </TodoList>
+      <TodoCount  completed={16} total={40}/>
+      <TodoButton/>
+
     </div>
   );
 }
